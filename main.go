25a4bbd3347511762dbb1d368f5fd93aab2f7a6e
Prathmesh Balsurkar <prathmeshb.code@gmail.com>
2026-03-31 18:36:21 +0530

    your message

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"learn_go/concurrency"
+)
+
 func main() {
+
+	nums := []int{1, 2, 3, 4, 5}
+	ch := make(chan int)
+	for _, num := range nums {
+		go concurrency.Squares(num, ch)
+
+	}
+
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(<-ch)
+	}
 }
